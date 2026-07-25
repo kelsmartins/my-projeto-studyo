a49862eb6b9a90381e8c2a675f673885810c0ad7
klss <kelsoglauber11@gmail.com>
2026-07-25 18:41:57 -0400

    Delete Message.tsx

--- src/components/chat/Message.tsx
+++
@@ -1,9 +0,0 @@
-export function Message(){
-    return (
-        <div className="p-2 bg-[#0023ff] rounded-lg overflow-y-auto">
-            <p className="text-white text-xs">
-                Esta é uma mensagem de exemplo.
-            </p>
-        </div>
-    )
-}
