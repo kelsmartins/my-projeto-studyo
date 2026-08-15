a6338d4d68e5e2ff10eab205d32b9b351053cdb7
klss <kelsoglauber11@gmail.com>
2026-08-15 14:52:52 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { MiniChat } from "../components/chat/MiniChat";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MiniChat />
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#F4F2EC]">
+      {/* <MiniChat /> */}
     </div>
   );
 }
