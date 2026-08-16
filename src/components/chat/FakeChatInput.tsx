9ce29cb115af0b14f29a5df2602f4ac3b6ac20d0
klss <kelsoglauber11@gmail.com>
2026-08-16 12:12:05 -0400

    Update FakeChatInput.tsx

--- src/components/chat/FakeChatInput.tsx
+++ src/components/chat/FakeChatInput.tsx
@@ -1,12 +1,17 @@
 import { Paperclip, Send } from "lucide-react";
 
-export function FakeChatInput() {
+type FakeChatInputProps = {
+    handleAddFiles: (newFiles: FileList) => void;
+    selectedFiles: File[];
+};
+
+export function FakeChatInput({ handleAddFiles, selectedFiles }: FakeChatInputProps) {
     return (
-        <div className="w-full h-9 bg-[#F5F0E6] rounded-xl shadow-inner flex">
+        <div className={`w-full h-10 bg-[#E3DDCE] ${selectedFiles.length > 0 ? 'rounded-b-xl' : 'rounded-xl'} shadow-lg flex`}>
     
             <label className="flex items-center justify-center w-10 h-full">
                 <Paperclip size={16} className="text-[#292524]/50"/>
-                <input type="file" multiple className="hidden" />
+                <input type="file" multiple className="hidden" onChange={(e) => e.target.files && handleAddFiles(e.target.files)} />
             </label>
             <input type="text" className="flex-1 h-full rounded-r-xl px-2 text-[#292524]/70 focus:outline-none text-sm" placeholder="Digite sua mensagem..." />
             <label className="flex items-center justify-center w-10 h-full">
