16e66ed3de5d0b8cde14ee88bbc9a76363b35767
klss <kelsoglauber11@gmail.com>
2026-08-16 19:05:30 -0400

    Update StudyContext.tsx

--- contexts/StudyContext.tsx
+++ contexts/StudyContext.tsx
@@ -1,5 +1,7 @@
+'use client'
+import { axios_api } from "@/src/api/axios_api";
 import { StudyType } from "@/types/StudyType";
-import {createContext, ReactNode, useState } from "react";
+import {createContext, ReactNode, useEffect, useState } from "react";
 
 type StudyContextType = {
     studies: StudyType[]
@@ -11,6 +13,11 @@ export const StudyContext = createContext({} as StudyContextType)
 export function StudyContextProvider({children}: {children: ReactNode}){
 
     const [studies, setStudies] = useState<StudyType[]>([])
+
+    async function getStudies(){
+        let res = await axios_api.get('/studies')
+        setStudies([...studies, res.data])
+    }
 
     function addStudy(newStudy: StudyType){
         setStudies([...studies, newStudy])
