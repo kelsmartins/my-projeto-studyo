251661691f4f32f06bf5991ba8dc63c8e1467068
klss <kelsoglauber11@gmail.com>
2026-08-16 12:12:11 -0400

    Update StudyDetails.tsx

--- src/components/studycard/StudyDetails.tsx
+++ src/components/studycard/StudyDetails.tsx
@@ -1,5 +1,3 @@
-import { StudySource } from "./StudySource";
-
 type StudyDetailsProps = {
     handleShowDetails: () => void;
 };
@@ -19,6 +17,18 @@ export function StudyDetails({ handleShowDetails }: StudyDetailsProps) {
                 </ul>
 
             </div>  
+        </div>
+    )
+}
+
+
+import { Link } from "lucide-react";
+
+export function StudySource() {
+    return (
+        <div className="size-24 bg-[#D3CDBE] rounded-xl flex flex-col justify-center p-4 overflow-hidden">
+            <Link className="text-[#ff0000] size-6 mx-auto" />
+            <span className="text-[#ff0000] text-xs truncate mt-1 break-words font-bold">atividade bla bla nao sei</span>
         </div>
     )
 }
