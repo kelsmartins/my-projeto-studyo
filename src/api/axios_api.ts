98325d68f0f876803070f67f624f245500c3fb82
klss <kelsoglauber11@gmail.com>
2026-08-16 19:05:34 -0400

    Update axios_api.ts

--- src/api/axios_api.ts
+++ src/api/axios_api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
- const axios_api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/studies'
- })
+export const axios_api = axios.create({
+    baseURL: 'http://127.0.0.1:5000'
+})
