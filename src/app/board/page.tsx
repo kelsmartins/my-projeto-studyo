f9c2f71f5cca7b63837f31787a61b6f69096a006
klss <kelsoglauber11@gmail.com>
2026-08-17 14:48:06 -0400

    Board Header

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,19 +1,43 @@
 'use client';
+import { FakeChat } from "@/src/components/chat/FakeChat";
 import { StudyCard } from "@/src/components/studycard/StudyCard";
-import { StudyDetails } from "@/src/components/studycard/StudyDetails";
+import { PlusCircle } from "lucide-react";
+import { useState } from "react";
 
 
 export default function Board() {
+
+  const [showFakeChat, setShowFakeChat] = useState(false);
+
+  function handleShowFakeChat(){
+    setShowFakeChat(!showFakeChat)
+  }
+
   return (
     <div className="flex flex-col justify-between min-h-screen bg-[#D3CDBE]">
 
-        <div className="w-full h-14 bg-red-500">
+        <div className="w-full h-14 p-6 flex items-center justify-between text-[#292524]">
+
+          <h2 className=" font-bold text-xl">
+            Mural de Cards
+          </h2>
+
+          <button 
+          className="flex items-center justify-center gap-1 px-2 py-1 bg-[#292524] rounded-lg text-[#D3CDBE] shadow-xl"
+          onClick={handleShowFakeChat}>
+            <PlusCircle size={18} className=" font-bold"/>
+            estudo
+          </button>
+
+          {showFakeChat && 
+            <FakeChat handleShowFakeChat={handleShowFakeChat} />
+          }
 
         </div>
 
-        <div className="flex-1 p-4">
+        <ul className="flex-1 p-4">
             <StudyCard />
-        </div>
+        </ul>
     </div>
   );
 }
