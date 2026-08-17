d177e54ef277094a7228778674443a7a0e7861d0
klss <kelsoglauber11@gmail.com>
2026-08-17 14:47:53 -0400

    ParseStudy function

--- src/components/chat/FakeChat.tsx
+++ src/components/chat/FakeChat.tsx
@@ -3,10 +3,21 @@ import { X } from "lucide-react";
 import { FakeChatInput } from "./FakeChatInput";
 import { FilesList } from "./FilesList";
 import { useState } from "react";
+import { useStudyContext } from "@/contexts/StudyContext";
+import { MessageBubble } from "./MessageBubble";
 
-export function FakeChat() {
+type FakeChatProps = {
+    handleShowFakeChat: ()=> void;
+}
 
+export function FakeChat({handleShowFakeChat} : FakeChatProps) {
+
+    const [message, setMessage] = useState("");
     const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+    const {parsedStudy} = useStudyContext();
+
+    const {handleParsedStudy} = useStudyContext();
 
     function handleAddFiles(newFiles: FileList) {
         for (let i = 0; i < newFiles.length; i++) {
@@ -15,11 +26,20 @@ export function FakeChat() {
         }
     }
 
+    function handleParse(){
+        if(message.trim() != ''){
+            handleParsedStudy(message, selectedFiles)
+        }
+    }
+
+
+
     return (
         <div className="w-screen h-screen bg-[#D3CDBE] absolute top-0 left-0 flex flex-col justify-center items-center">
 
             <div className="w-full h-10 bg-[#292524] fixed top-0">
-                <button>
+                <button
+                onClick={handleShowFakeChat}>
                     <X className="text-white absolute top-2 right-2" size={20} />
                 </button>
             </div>
@@ -29,12 +49,12 @@ export function FakeChat() {
             <div className="w-100 max-h-80 flex flex-col">
                 <ul className="flex-1 flex items-center justify-center mb-5">
 
-
+                <MessageBubble studyData={parsedStudy}/>
 
                 </ul>
 
                 <FilesList  selectedFiles={selectedFiles} />
-                <FakeChatInput handleAddFiles={handleAddFiles} selectedFiles={selectedFiles} />
+                <FakeChatInput handleAddFiles={handleAddFiles} selectedFiles={selectedFiles} setMessage={setMessage} handleParse={handleParse} />
 
             </div>  
         </div>
--- src/components/chat/FakeChatInput.tsx
+++ src/components/chat/FakeChatInput.tsx
@@ -1,11 +1,14 @@
+import { MaterialType } from "@/types/StudyType";
 import { Paperclip, Send } from "lucide-react";
 
 type FakeChatInputProps = {
     handleAddFiles: (newFiles: FileList) => void;
     selectedFiles: File[];
+    setMessage: (text: string) => void;
+    handleParse: () => void;
 };
 
-export function FakeChatInput({ handleAddFiles, selectedFiles }: FakeChatInputProps) {
+export function FakeChatInput({ handleAddFiles, selectedFiles, setMessage, handleParse }: FakeChatInputProps) {
     return (
         <div className={`w-full h-10 bg-[#E3DDCE] ${selectedFiles.length > 0 ? 'rounded-b-xl' : 'rounded-xl'} shadow-lg flex`}>
     
@@ -13,11 +16,19 @@ export function FakeChatInput({ handleAddFiles, selectedFiles }: FakeChatInputPr
                 <Paperclip size={16} className="text-[#292524]/50"/>
                 <input type="file" multiple className="hidden" onChange={(e) => e.target.files && handleAddFiles(e.target.files)} />
             </label>
-            <input type="text" className="flex-1 h-full rounded-r-xl px-2 text-[#292524]/70 focus:outline-none text-sm" placeholder="Digite sua mensagem..." />
-            <label className="flex items-center justify-center w-10 h-full">
+
+            <input 
+            type="text" 
+            className="flex-1 h-full rounded-r-xl px-2 text-[#292524]/70 focus:outline-none text-sm" 
+            placeholder="Digite sua mensagem..." 
+            onChange={e => setMessage(e.target.value)}
+            />
+            
+            <button 
+            className="flex items-center justify-center w-10 h-full"
+            onClick={handleParse}>
                 <Send size={16} className="text-[#292524]/50"/>
-                <input type="button" className="hidden" />
-            </label>
+            </button>
 
 
         </div>
