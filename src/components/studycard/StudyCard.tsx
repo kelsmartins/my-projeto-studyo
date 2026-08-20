7aa8e7a34d2c223d2e0678040ad4b5d2bd417d35
klss <kelsoglauber11@gmail.com>
2026-08-20 02:20:50 -0400

    Update StudyCard.tsx

--- src/components/studycard/StudyCard.tsx
+++ src/components/studycard/StudyCard.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { StudyDetails } from "./StudyDetails";
+import { StudyType } from "@/src/types/StudyType";
 
-export function StudyCard(){
+type StudyCardProps = {
+    studyData: StudyType
+}
+
+export function StudyCard({studyData}: StudyCardProps){
 
     const [showDetails, setShowDetails] = useState(false);
 
@@ -12,13 +17,13 @@ export function StudyCard(){
     return (
         <div className="w-70 h-40 bg-[#E3DDCE] flex justify-between rounded-xl shadow-xl">
 
-            <div className="w-4 h-full bg-[#ff0000] rounded-l-xl"></div>
+            <div className="w-4 h-full rounded-l-xl" style={{backgroundColor: studyData.color_hex}}></div>
 
             <div className="flex flex-col justify-between p-4">
 
                 <div className="w-full h-full flex flex-col">
-                    <h2 className="text-[#292524] font-bold mb-1 text-md max-h-9 overflow-hidden tracking-tighter leading-none">Study Card I dont know what am I doing but I think I am doing it wrong</h2>
-                    <h3 className="text-[#ff0000] text-xs font-bold">12/07/2004</h3>
+                    <h2 className="text-[#292524] font-bold mb-1 text-md max-h-9 overflow-hidden tracking-tighter leading-none">{studyData.title}</h2>
+                    <h3 className="text-[#ff0000] text-xs font-bold" style={{backgroundColor: studyData.color_hex}}>{studyData.date.toDateString()}</h3>
                 </div>
 
                 <div className="w-full h-10 flex items-center justify-between text-xs text-[#292524]">
