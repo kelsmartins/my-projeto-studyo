05d94bf09c34133fb6016aecab5c017b67a84b40
klss <kelsoglauber11@gmail.com>
2026-08-20 10:59:07 -0400

    Update StudyType.ts

--- src/types/StudyType.ts
+++ src/types/StudyType.ts
@@ -1,16 +1,14 @@
-import { UUID } from "crypto";
-
 export interface MaterialType {
     type: string | File;
-    name: string
+    name: string;
+    url?: string; // Dica: url como opcional pois o Supabase devolve ele!
 }
 
 export interface StudyType {
-    id: UUID,
-    title: string,
-    date: Date,
-    materials: MaterialType[]
+    id: string; // Trocado de UUID para string
+    title: string;
+    date: string; // Trocado de Date para string
+    material: MaterialType[]; // Trocado de materials para material
     color_name: string;  
     color_hex: string; 
-    
 }
