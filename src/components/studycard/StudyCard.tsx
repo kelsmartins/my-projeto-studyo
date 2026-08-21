2eef3b7c5f69baa7f3b7e5296984e52a3d61d633
klss <kelsoglauber11@gmail.com>
2026-08-21 02:37:54 -0400

    Update StudyCard.tsx

--- src/components/studycard/StudyCard.tsx
+++ src/components/studycard/StudyCard.tsx
@@ -15,23 +15,23 @@ export function StudyCard({studyData}: StudyCardProps){
     }
 
     return (
-        <div className="w-70 h-40 bg-[#E3DDCE] flex justify-between rounded-xl shadow-xl">
+        <div className="w-72 h-40 bg-[#E3DDCE] flex rounded-xl shadow-xl">
 
-            <div className="w-4 h-full rounded-l-xl" style={{backgroundColor: studyData.color_hex}}></div>
+            <div className="w-4 h-full rounded-l-xl" style={{backgroundColor: studyData.color_hex ? studyData.color_hex : '#292524' }}></div>
 
-            <div className="flex flex-col justify-between p-4">
+            <div className="flex-1 flex flex-col justify-between p-4">
 
                 <div className="w-full h-full flex flex-col">
                     <h2 className="text-[#292524] font-bold mb-1 text-md max-h-9 overflow-hidden tracking-tighter leading-none">{studyData.title}</h2>
-                    <h3 className="text-[#ff0000] text-xs font-bold" style={{backgroundColor: studyData.color_hex}}>{new Date(studyData.date).toLocaleDateString('pt-BR')}</h3>
+                    <h3 className="text-[#292524] text-xs font-bold" style={{color: studyData.color_hex}}>{studyData.date}</h3>
                 </div>
 
-                <div className="w-full h-10 flex items-center justify-between text-xs text-[#292524] gap-2">
+                <div className="w-full h-10 flex items-center justify-end text-xs text-[#292524] gap-4">
                     <button className="font-bold hover:underline hover:cursor-pointer"
                     onClick={handleShowDetails}>
                         ver mais
                     </button>
-                    <button className="px-3 py-2 rounded-lg font-semibold text-white" 
+                    <button className="px-3 py-2 rounded-lg font-semibold text-[#292524]" 
                     style={{backgroundColor: studyData.color_hex ? studyData.color_hex : '#292524' }}>
                         concluir
                     </button>
