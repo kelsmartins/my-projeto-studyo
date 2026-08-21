708b32ad48485906b774ec2240a0da1c5d46152a
klss <kelsoglauber11@gmail.com>
2026-08-21 02:37:45 -0400

    Update FilesList.tsx

--- src/components/chat/FilesList.tsx
+++ src/components/chat/FilesList.tsx
@@ -1,26 +1,44 @@
-import { File } from "lucide-react";
+import { MaterialType } from "@/src/types/StudyType";
+import { File, Link } from "lucide-react";
 
 type FilesListProps = {
-    selectedFiles: File[];
+    material: MaterialType[];
 };
 
-export function FilesList({ selectedFiles }: FilesListProps) {
+export function FilesList({ material }: FilesListProps) {
     return (
-        <ul className={`w-full h-10 bg-[#E3DDCE] p-2 gap-1 ${selectedFiles.length > 0 ? 'flex rounded-t-xl' : 'hidden'} shadow-lg overflow-x-auto overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
-            {selectedFiles.map((file, index) => (
-
-                <FileItem key={index} file={file} />
+        <ul className={`w-full h-10 bg-[#E3DDCE] p-2 gap-1 ${material.length > 0 ? 'flex rounded-t-xl' : 'hidden'} shadow-lg overflow-x-auto overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
+            {material.map((file, index) => (
+                <StudySource key={index} material={file} />
 
             ))}
         </ul>
     )}
 
 
-export function FileItem({ file }: { file: File }) {
-    return (
-        <li className="flex w-22 h-full px-2 py-2 bg-[#292524]/40 rounded-xl w-20 flex items-center justify-between gap-1">
-            <File size={13} className="text-[#000]/60 size-24"/>
-            <span className="text-[#000]/70 font-bold text-xs truncate">{file.name}</span>
-        </li>
-    )
+export default function StudySource({ material }: { material: MaterialType }) {
+    
+    if (material.type === 'file') {
+        return (
+            <li>
+                <a href={material.url} target="_blank" rel="noopener noreferrer">
+                    <File size={16}/>
+                    {material.name}
+                </a>
+            </li>
+        )
+    }
+
+    if (material.type === 'link') {
+        return (
+            <li>
+                <a href={material.name} target="_blank" rel="noopener noreferrer">
+                    <Link size={16}/>
+                    {material.name}
+                </a>
+            </li>
+        )
+    }
+
+    return null;
 }
