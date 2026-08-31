ffb01a5190e87a1876cd4e7ff610f549bcfe7b39
klss <kelsoglauber11@gmail.com>
2026-08-30 21:30:46 -0400

    Update StudyDetails.tsx

--- src/components/studycard/StudyDetails.tsx
+++ src/components/studycard/StudyDetails.tsx
@@ -4,27 +4,25 @@ import { Link, X } from "lucide-react";
 
 type StudyDetailsProps = {
     handleShowDetails: () => void;
-    studyData:  StudyType;
+    studyData: StudyType;
 };
 
 export function StudyDetails({ handleShowDetails, studyData }: StudyDetailsProps) {
 
-    const { deleteMaterial } = useStudyContext();
-
     return (
         <div className="w-screen h-screen backdrop-blur-xs bg-black/20 absolute top-0 left-0 flex justify-center items-center"
-        onClick={handleShowDetails}>
+            onClick={handleShowDetails}>
             <div className="w-100 min-h-120 bg-[#faf6ed] rounded-xl shadow-lg flex flex-col overflow-hidden" onClick={(e) => e.stopPropagation()}>
 
-                <div className="w-full h-1.5 rounded-t-xl" style={{backgroundColor: studyData.color_hex ? studyData.color_hex : '#292524' }}></div>
+                <div className="w-full h-1.5 rounded-t-xl" style={{ backgroundColor: studyData.color_hex ? studyData.color_hex : '#292524' }}></div>
 
                 <ul className="flex-1 p-4 overflow-y-auto">
-                    {studyData.material?.map((material) => 
-                        <StudySource key={material.id} material={material} borderColor={studyData.color_hex}/>
+                    {studyData.material?.map((material) =>
+                        <StudySource key={material.id} studyData={studyData} thisMaterial={material} />
                     )}
                 </ul>
 
-            </div>  
+            </div>
         </div>
     )
 }
@@ -32,17 +30,20 @@ export function StudyDetails({ handleShowDetails, studyData }: StudyDetailsProps
 
 
 type studySourceType = {
-    material: MaterialType;
-    borderColor: string
+    studyData: StudyType;
+    thisMaterial: MaterialType
 }
-export function StudySource({material, borderColor}: studySourceType) {
+export function StudySource({ studyData, thisMaterial }: studySourceType) {
+
+    const { deleteMaterial } = useStudyContext();
+
     return (
-        <div className="size-24 bg-[#F0ECE2] rounded-xl flex flex-col justify-center p-4 overflow-hidden" style={{ border: `1px solid ${borderColor ? borderColor : '#292524'}` }}>
-            <button className="flex items-center justify-center absolute">
-                <X className="size-6"/>
+        <div className="size-24 bg-[#F0ECE2] rounded-xl flex flex-col justify-center p-4 overflow-hidden relative" style={{ border: `1px solid ${studyData.color_hex ? studyData.color_hex : '#292524'}` }}>
+            <button className="absolute top-1 right-1 flex items-center justify-center size-6 rounded-md text-[#292524]/50 hover:bg-red-100 hover:text-red-600 transition-colors">
+                <X className="size-3.5" />
             </button>
-            <Link className="size-6 mx-auto" style={{color: borderColor ? borderColor : '#292524' }} />
-            <span className="text-xs truncate mt-1 break-words font-bold" style={{color: borderColor ? borderColor : '#292524' }}>{material.name}</span>
+            <Link className="size-6 mx-auto" style={{ color: studyData.color_hex ? studyData.color_hex : '#292524' }} />
+            <span className="text-xs truncate mt-1 break-words font-bold" style={{ color: studyData.color_hex ? studyData.color_hex : '#292524' }}>{thisMaterial.name}</span>
         </div>
     )
 }
