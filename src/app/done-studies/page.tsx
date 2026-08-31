59c80bb4e1cdf469eca7b1b761cbc249ee1b44bd
klss <kelsoglauber11@gmail.com>
2026-08-31 11:19:24 -0400

    Create page.tsx

---
+++ src/app/done-studies/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { StudyCard } from "@/src/components/studycard/StudyCard";
+import { useStudyContext } from "@/src/contexts/StudyContext";
+import { Trash2Icon } from "lucide-react";
+
+
+export default function DoneStudies() {
+
+
+    const { doneStudies } = useStudyContext()
+
+
+    return (
+        <div className="flex flex-col min-h-screen">
+
+            <div className="w-full px-6 py-4 flex items-center justify-between text-[#292524]">
+
+                <h2 className="font-bold text-xl">
+                    Estudos concluídos
+                </h2>
+
+                <button
+          className="flex items-center justify-center gap-1 px-3 py-2 bg-[#292524] rounded-lg text-[#D3CDBE] text-xs shadow-xl"
+          onClick={()=>{}}>
+          <Trash2Icon size={14} className="font-bold" />
+          todos
+        </button>
+
+            </div>
+
+            <ul className=" px-6 py-4 flex">
+                {doneStudies.map((study, index) => (
+                    <StudyCard key={index} studyData={study} />
+                ))}
+            </ul>
+
+        </div>
+    );
+}
