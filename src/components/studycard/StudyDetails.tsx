ed6d001986b555addaf4e3ff3bb6bb1a25fe4cae
klss <kelsoglauber11@gmail.com>
2026-09-07 02:31:19 -0400

    Update StudyDetails.tsx

--- src/components/studycard/StudyDetails.tsx
+++ src/components/studycard/StudyDetails.tsx
@@ -15,9 +15,14 @@ export function StudyDetails({ handleShowDetails, studyData }: StudyDetailsProps
 
                 <div className="w-full h-2 rounded-t-xl" style={{ backgroundColor: studyData.color_hex ? studyData.color_hex : '#292524' }}></div>
 
+                <h2 className="text-sm font-bold m-auto mt-2 text-[#292524]">{studyData.title}</h2>
+                <h3 className="text-sm m-auto font-bold" style={{ color: studyData.color_hex ? studyData.color_hex : '#292524' }}>
+                    {studyData.date}
+                </h3>
+
                 <ul className="flex-1 p-4 overflow-y-auto flex flex-wrap gap-3 justify-start">
                     {studyData.material?.map((material, index ) =>
-                        <Material key={index} thisColor={studyData.color_hex} studyId={studyData.id} thisMaterial={material} />
+                        <Material key={index} thisColor={studyData.color_hex} studyId={studyData.id} thisMaterial={material} handleClose={handleShowDetails} />
                     )}
                 </ul>
 
