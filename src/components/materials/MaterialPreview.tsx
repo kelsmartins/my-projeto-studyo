75d1eba53e8af95c52b93b3bf512030af9e2ecd1
klss <kelsoglauber11@gmail.com>
2026-09-07 12:01:38 -0400

    Update MaterialPreview.tsx

--- src/components/materials/MaterialPreview.tsx
+++ src/components/materials/MaterialPreview.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { MaterialType } from '@/src/types/StudyType';
-import dynamic from 'next/dynamic';
 
-const FilePreview = dynamic(() => import('reactjs-file-preview'), { ssr: false });
 
 type MaterialPreviewProps = {
     materialData: MaterialType;
@@ -13,13 +11,7 @@ type MaterialPreviewProps = {
 export function MaterialPreview({ materialData, handleClose }: MaterialPreviewProps){
 
     return (
-            <div
-      className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center p-4 overflow-hidden backdrop-"
-      onClick={handleClose}>
-        <div className="pointer-events-none w-60 h-80 shadow-2xl">
-          <FilePreview preview={materialData.url ?? null} />
-        </div>
-      </div>
+          <></> 
     )
 
 }
