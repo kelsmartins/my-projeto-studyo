3975193529976f81bbbf7c1743caa4e972b9ae03
klss <kelsoglauber11@gmail.com>
2026-09-18 08:09:31 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-'use client'
+// 'use client'
+
+// import { MaterialViewer } from "../components/materialviewers/MaterialViewer";
 
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen p-4">
+// export default function Home() {
+//   return (
+//     <div className="flex min-h-screen p-4 flex items-center justify-between">
 
-    </div>
-  );
-}
+//       <MaterialViewer />
+
+//     </div>
+//   );
+// }
