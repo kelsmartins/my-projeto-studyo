8d9881b364cb2be36f7da6ac922c056a97945698
klss <kelsoglauber11@gmail.com>
2026-09-18 08:10:01 -0400

    Create MaterialViewer.tsx

---
+++ src/components/materialviewers/MaterialViewer.tsx
@@ -0,0 +1,18 @@
+// "use client";
+
+// import { MaterialType } from "@/src/types/StudyType";
+
+
+// export type MaterialViewerProps = {
+//   materialData: MaterialType;
+//   handleClose: () => void;
+// };
+
+// export function MaterialViewer({ materialData, handleClose }: MaterialViewerProps) {
+//   return (
+//     <div
+//       className="w-[50%] h-screen bg-[#292524]/20 fixed top-0 left-0 flex justify-center items-center p-4 overflow-y-auto">
+
+//     </div>
+//   )
+// }
