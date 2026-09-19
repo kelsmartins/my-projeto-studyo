b224bf05fba570e35790ae64b843f33861929f39
klss <kelsoglauber11@gmail.com>
2026-09-19 09:08:32 -0400

    Update MaterialList.tsx

--- src/components/chat/MaterialList.tsx
+++ src/components/chat/MaterialList.tsx
@@ -7,34 +7,32 @@ type FilesListProps = {
 
 export function MaterialList({ material }: FilesListProps) {
 
-    const {deleteMaterial} = useStudyContext();
-
     return (
-        <ul className={`w-full h-10 bg-[#faf6ed] p-2 gap-1 ${material.length > 0 ? 'flex rounded-t-xl' : 'hidden'} shadow-lg overflow-x-auto overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
+        <ul className={`w-full h-10 bg-[#F9FBFC] p-2 gap-1 ${material.length > 0 ? 'flex rounded-t-xl' : 'hidden'} shadow-lg border border-[#292524]/15 border-b-none overflow-x-auto overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
             {material.map((file, index) => (
-                <StudySource key={index} file={file} index={index} />
+                <MaterialItem key={index} file={file} index={index} />
 
             ))}
         </ul>
     )}
 
-type StudySourceProps = {
+type MaterialItemProps = {
     file: File;
     index: number;
 }
 
-export default function StudySource({ file, index }:  StudySourceProps) {
+export default function MaterialItem({ file, index }:  MaterialItemProps) {
 
     const { deleteCurrentSelectedMaterial } = useStudyContext();
     
         return( 
             <li
-                className="bg-[#F9FBFC] text-xs flex gap-2 p-2 w-26 h-full items-center justify-center rounded-md">
+                className="bg-[#FFFFFE] text-xs flex gap-2 p-2 w-26 h-full items-center justify-center rounded-md">
                     <File size={26} className="text-black"/>
                     <span className="truncate">{file.name}</span>
-                    <button className="bg-[#D3CDBE] rounded-full size-4 flex items-center cursor-pointer"
+                    <button className="bg-transparent rounded-full size-4 flex items-center cursor-pointer"
                     onClick={()=> deleteCurrentSelectedMaterial(index)}>
-                        <X className="w-full h-full text-[#292524]/50"/>
+                        <X className="w-full h-full size-full text-[#292524]/50"/>
                     </button>
             </li>
         )
