36998a7f88b7b0d37e3929111a8260c48d103a7a
klss <kelsoglauber11@gmail.com>
2026-09-19 11:15:07 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,6 @@ import { StudyType } from "../types/StudyType";
 export default function Home() {
 
 
-  const study: StudyType = {
-    id: 'asdaf-dsfrgrg-dgrtg',
-    title: 'Revisar Redacao',
-    done: false,
-    color_hex: '#292524',
-    color_name: 'brown',
-    date: '12 de Outubro de 2026',
-    material: [
-      { id: '`7f3c2a91-6d84-4b17-9e52-a8c71f0d3b46`', type: 'string', name: 'ola', url: 'https://codeshack.io/html-viewer/' }
-    ]
-
-  }
-
   return (
     <div className="flex-1 min-h-[calc(100vh-60px)] bg-blue-200">
 
