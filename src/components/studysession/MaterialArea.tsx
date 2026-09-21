76f886a75bb445f46d1d2f1789ed70a5e31b5b6a
klss <kelsoglauber11@gmail.com>
2026-09-20 23:43:45 -0400

    study session

---
+++ src/components/studysession/MaterialArea.tsx
@@ -0,0 +1,5 @@
+export function MaterialArea(){
+    return (
+        <main className="h-full w-[70%] min-w-0 flex-1 bg-yellow-400"></main>
+    )
+}
---
+++ src/components/studysession/SessionHeader.tsx
@@ -0,0 +1,7 @@
+export function SessionHeader(){
+    return (
+        <header className="w-full h-[70px] bg-orange-200">
+
+        </header>
+    )
+}
