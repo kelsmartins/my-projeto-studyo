f1d9e83075095732518fddec2fa997e3928dfd48
klss <kelsoglauber11@gmail.com>
2026-09-21 18:37:19 -0400

    Chevron Down/Up

--- src/components/studyitems/StudyItem.tsx
+++ src/components/studyitems/StudyItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { StudyMaterials } from "./StudyMaterials";
 import { StudyType } from "@/src/types/StudyType";
 import { useStudyContext } from "@/src/contexts/StudyContext";
-import { Check, ChevronDown, Play, RotateCcw, StepBack, Trash } from "lucide-react";
+import { Check, ChevronDown, ChevronUp, Play, RotateCcw, StepBack, Trash } from "lucide-react";
 
 type StudyItemProps = {
     studyData: StudyType
@@ -37,13 +37,18 @@ export function StudyItem({ studyData }: StudyItemProps) {
                 </div>
 
                 <h2 className="text-[#292524] font-bold mb-1 text-md truncate">{studyData.title}</h2>
-                
+
                 <button className="text-xs font-bold mt-2 flex gap-1"
                     style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }}
                     onClick={handleShowMaterials}>
-                    <ChevronDown className="text-green-600 size-4 font-bold"
-                        style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
-                    2 materiais
+                    {!showMaterials ?
+                        <ChevronDown className="text-green-600 size-4 font-bold"
+                            style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
+                        :
+                        <ChevronUp className="text-green-600 size-4 font-bold"
+                            style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
+                    }
+                    {studyData.material.length > 1 ? `${studyData.material.length} materiais` : '1 material'}
                 </button>
 
 
@@ -53,13 +58,16 @@ export function StudyItem({ studyData }: StudyItemProps) {
 
                 <div className="h-full w-30 flex flex-col items-center justify-center gap-4 border-l border-l-[#292524]/20">
 
-                    <Link className="rounded-full flex items-center justify-center size-8"
+                    <Link className="rounded-full flex items-center justify-center size-6 border border-[#292524]/30"
+                        style={{ border: '1px solid', borderColor: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }}
                         href={`/study-session/${studyData.id}`}>
-                        <Play />
+                        <Play className="text-green-600 size-3"
+                            style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
                     </Link>
 
                     <button
-                        className="rounded-full flex items-center justify-center size-6 border border-[#292524]/30"
+                        className="rounded-full flex items-center justify-center size-6"
+                        style={{ border: '1px solid', borderColor: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }}
                         onClick={() => checkDoneStudy(studyData.id)}>
                         < Check className="text-green-600 size-3"
                             style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
@@ -68,9 +76,10 @@ export function StudyItem({ studyData }: StudyItemProps) {
 
                 :
 
-                <div className="h-full w-30 flex flex-col items-center justify-center gap-4 border-l border-l-[#292524]/20">
+                <div className="h-full w-30 flex flex-col items-center justify-center gap-4 ">
 
                     <button className="rounded-full flex items-center justify-center size-6 border border-[#292524]/30"
+                        style={{ border: '1px solid', borderColor: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }}
                         onClick={() => checkDoneStudy(studyData.id)}>
                         <RotateCcw className="text-green-600 size-3"
                             style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
@@ -78,6 +87,7 @@ export function StudyItem({ studyData }: StudyItemProps) {
 
                     <button
                         className="rounded-full flex items-center justify-center size-6 border border-[#292524]/30"
+                        style={{ border: '1px solid', borderColor: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }}
                         onClick={() => deleteStudy(studyData.id)}>
                         <Trash className="text-green-600 size-3"
                             style={{ color: studyData.color_hex ? `${studyData.color_hex}` : '#292524' }} />
