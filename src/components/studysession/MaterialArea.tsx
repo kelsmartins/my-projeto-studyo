82dda773f746449728221c1b32ca60d632ffc85a
klss <kelsoglauber11@gmail.com>
2026-09-21 14:32:02 -0400

    Update MaterialArea.tsx

--- src/components/studysession/MaterialArea.tsx
+++ src/components/studysession/MaterialArea.tsx
@@ -8,9 +8,13 @@ type MaterialAreaProps = {
 
 export function MaterialArea({materials}: MaterialAreaProps){
     return (
-        <main className="h-full w-[70%] bg-blue-400 flex overflow-y-auto">
-            <SessionMaterials />
-            <PdfViewer materialData={materials[0]}/>
-        </main>
+        <div className='bg-purple-200 h-full w-[70%] flex flex-col'>
+          <SessionMaterials />
+
+          <div className='flex-1 bg-green-200 flex items-center justify-center p-10 overflow-auto'>
+            <PdfViewer materialData={materials[0]} />
+          </div>
+    
+        </div>
     )
 }
