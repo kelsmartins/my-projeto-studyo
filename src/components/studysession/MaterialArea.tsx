b4cafee6043e3e3d3dad2fe6d1215574755646a7
klss <kelsoglauber11@gmail.com>
2026-09-20 23:51:04 -0400

    Update MaterialArea.tsx

--- src/components/studysession/MaterialArea.tsx
+++ src/components/studysession/MaterialArea.tsx
@@ -1,5 +1,5 @@
 export function MaterialArea(){
     return (
-        <main className="h-full w-[70%] min-w-0 flex-1 bg-yellow-400"></main>
+        <main className="h-full w-[70%]  flex-1 bg-yellow-400"></main>
     )
 }
