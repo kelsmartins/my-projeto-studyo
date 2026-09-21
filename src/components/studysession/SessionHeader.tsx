2a349cb9abb130a8ed8cb6a05184dd51a111949d
klss <kelsoglauber11@gmail.com>
2026-09-21 14:52:10 -0400

    Update SessionHeader.tsx

--- src/components/studysession/SessionHeader.tsx
+++ src/components/studysession/SessionHeader.tsx
@@ -1,6 +1,27 @@
-export function SessionHeader(){
+import { ArrowLeft, ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+type SessionHeaderProps={
+    title: string;
+}
+
+export function SessionHeader({title}: SessionHeaderProps){
     return (
-        <header className="w-full h-[60px] bg-orange-200">
+        <header className="w-full h-[60px] flex items-center justify-start p-4 gap-4">
+
+            <Link 
+                className="flex flex items-center gap-1"
+                href='/'>
+                    <ArrowLeft className="text-[#292524]/70 size-4 font-bold" />
+                    <span className="text-[#292524]/70 font-bold text-sm">Voltar</span>
+            </Link>
+
+            <span className="w-[1.5px] h-[50%] bg-[#292524]/50"></span>
+
+            <h2 
+                className="text-[#292524]/70 font-bold text-sm">
+                    {title}
+            </h2>
 
         </header>
     )
