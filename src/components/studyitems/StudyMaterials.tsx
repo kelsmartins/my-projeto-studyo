a4b4c24c5b2a19991c3943aee421f7ae469b0b01
klss <kelsoglauber11@gmail.com>
2026-09-21 18:36:53 -0400

    Update StudyMaterials.tsx

--- src/components/studyitems/StudyMaterials.tsx
+++ src/components/studyitems/StudyMaterials.tsx
@@ -9,7 +9,7 @@ type StudyMaterialsProps = {
 export function StudyMaterials({ handleShowDetails, studyData }: StudyMaterialsProps) {
 
     return (
-            <ul className="absolute -bottom-20 left-2 w-[72%] h-25 z-10 rounded-md shadow-lg overflow-y-hidden bg-[#fff] flex flex-col p-2 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+            <ul className="absolute -bottom-20 left-0 w-[90%] h-25 z-5 rounded-md shadow-lg overflow-y-hidden bg-[#fff] flex flex-col p-2 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
                     {studyData.material?.map((material, index ) =>
                         <MaterialItem 
                             key={index} 
