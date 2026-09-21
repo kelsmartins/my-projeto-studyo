31a3a02e84b5dbd315a30ed502e98efa773800bd
klss <kelsoglauber11@gmail.com>
2026-09-21 00:43:48 -0400

    Update SessionHeader.tsx

--- src/components/studysession/SessionHeader.tsx
+++ src/components/studysession/SessionHeader.tsx
@@ -1,6 +1,6 @@
 export function SessionHeader(){
     return (
-        <header className="w-full h-[70px] bg-orange-200">
+        <header className="w-full h-[60px] bg-orange-200">
 
         </header>
     )
